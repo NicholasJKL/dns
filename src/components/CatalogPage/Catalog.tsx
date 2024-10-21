4d7c18df0b26fd3abbe57c6ff478e5b097c77377
NicholasJKL <matveev_andrey2003@mail.ru>
2024-10-21 15:48:33 +0300

    Added Categories

--- src/components/CatalogPage/Catalog.tsx
+++ src/components/CatalogPage/Catalog.tsx
@@ -1,14 +1,45 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useRef, ChangeEvent, MouseEvent, RefObject } from 'react';
 
 import CatalogElement from './CatalogElement';
 
 import '../../styles.css';
 
 const Catalog: FC = () => {
-    return (
-        <div className='catalog-content'>
-            <CatalogElement></CatalogElement>
 
+    const [isActive, setActive] = useState<boolean>(false);
+    const search = useRef<HTMLFormElement>(null);
+
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        if (value) {
+            setActive(true);
+        }
+        else {
+            setActive(false);
+        }
+    }
+
+    const handleClear = (e: MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        search.current?.reset();
+        setActive(false);
+    }
+
+    return (
+        <div>
+            <form className='catalog-search' ref={search}>
+                <input type="search" placeholder='Поиск' onChange={handleInput} />
+                <button type='reset' className={isActive ? 'catalog-clear-button' : 'catalog-clear-button hidden'} onClick={handleClear}></button>
+                <button className='catalog-search-button'></button>
+            </form>
+            <div className='catalog-content'>
+                <CatalogElement></CatalogElement>
+                <CatalogElement></CatalogElement>
+                <CatalogElement></CatalogElement>
+                <CatalogElement></CatalogElement>
+                <CatalogElement></CatalogElement>
+                <CatalogElement></CatalogElement>
+            </div>
         </div>
     );
 };
--- src/components/CatalogPage/CatalogElement.tsx
+++ src/components/CatalogPage/CatalogElement.tsx
@@ -5,9 +5,14 @@ import '../../styles.css';
 const CatalogElement: FC = () => {
     return (
         <div className='catalog-element'>
-            <div className='catalog-element-img'><img src="" alt="Картинка" /></div>
-            <div className='catalog-element-title'><p>Товар</p></div>
-            <div className='catalog-element-buy'></div>
+            <div className='catalog-element-click'>
+                <div className='catalog-element-img'><img src={require('../../img/iphone6-item.jpg')} alt="Картинка" /></div>
+                <div className='catalog-element-title'><p>iPhone 6</p></div>
+            </div>
+            <div className='catalog-element-buy'>
+                <p>400₽</p>
+                <button></button>
+            </div>
         </div>
     );
 };
