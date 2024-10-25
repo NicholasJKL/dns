ab3a41794fe05ec1bd167a143654d0594873021b
NicholasJKL <matveev_andrey2003@mail.ru>
2024-10-25 22:01:06 +0300

    Added Search Message

--- src/components/CatalogPage/Catalog.tsx
+++ src/components/CatalogPage/Catalog.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useState, useRef, ChangeEvent, MouseEvent } from 'react';
+import React, { FC, useState, useRef, ChangeEvent, MouseEvent, useEffect } from 'react';
 
 import CatalogElement from './CatalogElement';
+import Item from '../../models/Item';
 
 import '../../styles/common_styles.css';
 import '../../styles/catalog_styles.css';
@@ -9,11 +10,28 @@ import '../../styles/catalog_styles.css';
 const Catalog: FC = () => {
 
     const [isActive, setActive] = useState<boolean>(false);
+    const [items, setItems] = useState<Item[]>([]);
+    const [searchValue, setSearchValue] = useState<string>('');
     const search = useRef<HTMLFormElement>(null);
+
+    useEffect(() => {
+        setItems([
+            { item_name: 'iPhone 6', item_price: '19 999', image_path: 'img/iphone6-item.jpg' },
+            { item_name: 'iPhone SE', item_price: '14 999', image_path: 'img/iphoneSE-item.jpg' },
+            { item_name: 'Intel i5-12400F', item_price: '28 999', image_path: 'img/intel-processor.jpg' },
+            { item_name: 'AMD Ryzen 5 7500F', item_price: '25 499', image_path: 'img/amd-processor.jpg' },
+            { item_name: 'Dark Project Keyboard', item_price: '11 099', image_path: 'img/darkproject-keyboard.jpg' },
+            { item_name: 'Huawei MateBook', item_price: '44 999', image_path: 'img/huawei-matebook.jpg' },
+            { item_name: 'Xiaomi TV', item_price: '27 999', image_path: 'img/xiaomi-tv.jpg' },
+            { item_name: 'MSI RTX 4060', item_price: '58 499', image_path: 'img/msi-graphiccard.jpg' }]);
+
+    }, []);
+
 
     const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
         const { value } = e.target;
         if (value) {
+            setSearchValue(value);
             setActive(true);
         }
         else {
@@ -30,20 +48,23 @@ const Catalog: FC = () => {
     return (
         <div>
             <form className='catalog-search' ref={search}>
-                <input type="search" placeholder='Поиск' onChange={handleInput} />
+                <input type="search" placeholder='Поиск' maxLength={64} onChange={handleInput} />
                 <button type='reset' className={isActive ? 'catalog-clear-button' : 'catalog-clear-button hidden'} onClick={handleClear}></button>
                 <button className='catalog-search-button'></button>
             </form>
-            <div className='catalog-content'>
-                <CatalogElement item_name='iPhone 6' item_price='19 999' image_path={process.env.PUBLIC_URL + 'img/iphone6-item.jpg'}></CatalogElement>
-                <CatalogElement item_name='iPhone SE' item_price='14 999' image_path={process.env.PUBLIC_URL + 'img/iphoneSE-item.jpg'}></CatalogElement>
-                <CatalogElement item_name='Intel i5-12400F ' item_price='28 999' image_path={process.env.PUBLIC_URL + 'img/intel-processor.jpg'}></CatalogElement>
-                <CatalogElement item_name='AMD Ryzen 5 7500F' item_price='25 499' image_path={process.env.PUBLIC_URL + 'img/amd-processor.jpg'}></CatalogElement>
-                <CatalogElement item_name='Dark Project Keyboard' item_price='11 099' image_path={process.env.PUBLIC_URL + 'img/darkproject-keyboard.jpg'}></CatalogElement>
-                <CatalogElement item_name='HUAWEI Matebook' item_price='44 999' image_path={process.env.PUBLIC_URL + 'img/huawei-matebook.jpg'}></CatalogElement>
-                <CatalogElement item_name='Xiaomi TV' item_price='27 999' image_path={process.env.PUBLIC_URL + 'img/xiaomi-tv.jpg'}></CatalogElement>
-                <CatalogElement item_name='MSI RTX 4060' item_price='58 499' image_path={process.env.PUBLIC_URL + 'img/msi-graphiccard.jpg'}></CatalogElement>
-            </div>
+
+            {(isActive) ?
+                <div className='search-result'>
+                    <p><b>Результаты поиска</b><br></br>
+                        Вы искали: {searchValue}</p>
+                </div>
+                :
+                <div className='catalog-content'>
+                    {items.map((item) => {
+                        return (<CatalogElement item_name={item.item_name} item_price={item.item_price} image_path={item.image_path}></CatalogElement>)
+                    })}
+                </div>
+            }
             <div className='catalog-pagination'>
                 <button className='catalog-pagination-left'></button>
                 <div className='catalog-pagination-current'><p>1</p></div>
---
+++ src/models/Item.ts
@@ -0,0 +1,7 @@
+type Item = {
+    item_name: string,
+    item_price: string,
+    image_path: string
+}
+
+export default Item;
