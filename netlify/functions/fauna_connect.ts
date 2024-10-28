c3ddb071958370685ee2aff6849806318c34a658
NicholasJKL <matveev_andrey2003@mail.ru>
2024-10-28 13:35:05 +0300

    Update fauna_connect.ts

--- netlify/functions/fauna_connect.ts
+++ netlify/functions/fauna_connect.ts
@@ -7,15 +7,16 @@ const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET || '' });
 
 exports.handler = async (event, context) => {
   try {
-    const data  = await client.query(
+    const data = await client.query(
       q.Create(q.Collection('Items'), { data: { name: 'Test Record' } })
     );
-
+    console.log('successful');
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Record created successfully!', data }),
     };
   } catch (error) {
+    console.log('error');
     console.error(error);
     return {
       statusCode: 500,
