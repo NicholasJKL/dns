4dd347fadc21b2841f7dd399f2ec171c74b98157
NicholasJKL <matveev_andrey2003@mail.ru>
2024-11-06 16:12:25 +0300

    Update db_test.ts

--- src/test/db_test.ts
+++ src/test/db_test.ts
@@ -1,15 +1,17 @@
 import { Client, fql, FaunaError } from "fauna";
 import User from "../models/User";
 
+class UserExistError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = 'UserExistError';
+    }
+}
 
 const client = new Client({
     secret: `fnAFuzZAwbAAyh_qRiZxFypuhT5B2m7y7WG-Z6PG`
 });
 
-const getAllItemsDb = async () => null;
-const getItemByIdDb = async (params: { item_id: number | string }) => null;
-const getSearchingItemsDb = async (params: { searchPrompt: string }) => null;
-const createUserDb = async (params: User) => null;
 const getUserDb = async (params: User): Promise<User> => {
     const query = fql`
     Users.firstWhere(user => user.user_email == ${params.user_email})`;
@@ -19,9 +21,84 @@ const getUserDb = async (params: User): Promise<User> => {
         throw new Error(`Неправильная почта или неправильный пароль`);
     }
     if (response.data.user_password === params.user_password) {
+
+const getAllItemsDb = async (): Promise<any> => {
+    try {
+        const query = fql`  
+        Items.all()`;
+        const response = await client.query(query);
+        console.log(response.data)
         return response.data;
+
+    } catch (error) {
+        if (error instanceof FaunaError) {
+            console.log(error);
+        }
     }
-    throw new Error(`Ошибка авторизации`);
 }
 
-export { getAllItemsDb, getItemByIdDb, getSearchingItemsDb, createUserDb, getUserDb };
+const getItemByIdDb = async (params: any): Promise<any> => {
+    try {
+        const query = fql`  
+            Items.firstWhere(item => item.id == ${params.item_id})`;
+        const response = await client.query(query);
+        return response.data;
+    }
+    catch (error) {
+        if (error instanceof FaunaError) {
+            console.log(error);
+        }
+    }
+}
+
+const getSearchingItemsDb = async (params: any): Promise<any> => {
+    try {
+        const query = fql`  
+        Items.where(.item_name.toLowerCase().includes(${params.searchPrompt}))`;
+        const response = await client.query(query);
+        return response.data;
+    }
+    catch (error) {
+        if (error instanceof FaunaError) {
+            console.log(error);
+        }
+    }
+}
+
+const createUserDb = async (user: User): Promise<any> => {
+    try {
+
+        const sameUser = fql`
+        Users.firstWhere(user => user.user_email == ${user.user_email})`
+        const responseSameUser = await client.query(sameUser);
+
+
+        if (responseSameUser.data !== null) {
+            console.log(responseSameUser.data)
+            throw (new UserExistError('Пользователь с такой почтой уже существует'));
+        }
+
+        const query = fql`  
+        Users.create({
+            user_email: ${user.user_email},
+            user_password: ${user.user_password},
+            user_name: "-",
+            user_phone: "-"
+        })`;
+        const response = await client.query(query);
+        return response.data;
+    }
+    catch (error) {
+        if (error instanceof FaunaError) {
+            console.log(error);
+        }
+        if (error instanceof UserExistError) {
+            throw error;
+        }
+    }
+}
+
+
+
+
+export { getAllItemsDb, getItemByIdDb, getSearchingItemsDb, createUserDb };
